880e7684d8032cdc3b0d70fdc1825ed0ae4bbf13
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2020-05-01 23:29:37 +0900

    setting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,43 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
+  
   def index
 
+  end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @posts = Post.create(post_params)
+    @posts.save
+    # redirect_to root_path
+  end
+
+  def show
+    @posts = Post.includes(:user).order("created_at DESC")
+  end
+
+  def edit
+
+  end
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  def search
+    @posts = Post.includes(:user).order("created_at DESC")
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users 
   root to: "posts#top"
-  resources :posts, only: :index
+  resources :posts, only: [:index,:new, :create, :show, :edit, :destroy] do
+    member do
+      get 'search'
+    end
+  end
 end
